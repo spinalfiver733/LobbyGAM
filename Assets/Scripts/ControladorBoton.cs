3586cb3d510ba4e647fde98482fa7230fbe3b8a7
Luis Enrique Vazquez Ortiz <spinalfiver733@gmail.com>
2025-01-10 17:18:57 -0600

    cambios en todos los modelos que se utilizaran para presentación con el alcalde

--- Assets/Scripts/ControladorBoton.cs
+++ Assets/Scripts/ControladorBoton.cs
@@ -3,17 +3,17 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class ControladorBoton : MonoBehaviour
 {
-    public ControladorCamino controladorCamino; // Referencia al script del camino
+    public ControladorCamino controladorCamino;
 
     private UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable interactable;
 
     void Start()
     {
         interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable>();
-        interactable.selectEntered.AddListener(OnBotonPresionado);
     }
 
-    private void OnBotonPresionado(SelectEnterEventArgs args)
+    // Cambiamos este m�todo a p�blico
+    public void OnBotonPresionado(BaseInteractionEventArgs args)
     {
         controladorCamino.CambiarEstadoCamino();
     }
